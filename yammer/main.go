43da077402c2ceedff4bf2962c0a0d321d041c9a
Brian Starkey <stark3y@gmail.com>
2017-03-10 21:16:48 +0000

    main: Add RateLimitedClient
    Add a http.Client subclass which has request rate limiting.
    
    FIXME: This should definitely be split out somewhere else.

--- yammer/main.go
+++ yammer/main.go
@@ -1,6 +1,78 @@
 package yammer
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type RateLimitedClient struct {
+	http.Client
+	ticker *time.Ticker
+	nreqs int
+	period time.Duration
+	throttle chan time.Time
+}
+
+func (r *RateLimitedClient) Do(req *http.Request) (resp *http.Response, err error) {
+	<-r.throttle
+	return r.Client.Do(req)
+}
+
+func (r *RateLimitedClient) Get(url string) (resp *http.Response, err error) {
+	<-r.throttle
+	return r.Client.Get(url)
+}
+
+func (r *RateLimitedClient) Head(url string) (resp *http.Response, err error) {
+	<-r.throttle
+	return r.Client.Head(url)
+}
+
+func (r *RateLimitedClient) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
+	<-r.throttle
+	return r.Client.Post(url, contentType, body)
+}
+
+func (r *RateLimitedClient) PostForm(url string, data url.Values) (resp *http.Response, err error) {
+	<-r.throttle
+	return r.Client.PostForm(url, data)
+}
+
+func NewRateLimitedClient(nreqs int, period time.Duration) *RateLimitedClient {
+	r := RateLimitedClient{
+		Client: http.Client{},
+		nreqs: nreqs,
+		period: period,
+		throttle: make(chan time.Time, nreqs),
+	}
+
+	r.ticker = time.NewTicker(r.period)
+	n := r.nreqs
+	for n > 0 {
+		r.throttle <- time.Now()
+		n--
+	}
+
+	go func() {
+		for {
+			select {
+			case <-r.ticker.C:
+				n := r.nreqs
+				for n > 0 {
+					select {
+					case r.throttle <- time.Now():
+					default:
+					n--
+					}
+				}
+			}
+		}
+	}()
+
+	return &r
+}
 
 type Client struct {
 	baseURL     string
